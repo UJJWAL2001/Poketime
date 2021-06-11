62ae61720eb076a70b39e71e8fad1af00090a675
UJJWAL CHAUDHARY <ujjwalanju2001@gmail.com>
2021-06-11 14:22:57 +0530

    fixed an error

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,45 +1,44 @@
-import React,{ Component } from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import Pokeball from '../pokeball.png'
-import { connect } from 'react-redux' // act as glue b/w react and redux 
+import { connect } from 'react-redux'
 
 class Home extends Component {
-    render(){
-        console.log(this.props)
-        const { posts } = this.props 
-        const postList = posts.length ? (
-        posts.map(post => {
-            return (
-            <div className="post card" key={post.id}>
-                <img src={Pokeball}  />
-                <div className="card-content">
-                    <Link to={'/'+post.id} className="red-text">
-                        <span className="card-title">{post.title}</span>
-                    </Link>
-                <p>{post.body}</p>
-                </div>
+  render(){
+    const { posts } = this.props
+    const postList = posts.length ? (
+      posts.map(post => {
+        return (
+          <div className="post card" key={post.id}>
+            <img src={Pokeball} alt="A Pokeball" />
+            <div className="card-content">
+              <Link to={'/' + post.id}>
+                <span className="card-title red-text">{post.title}</span>
+              </Link>
+              <p>{post.body}</p>
             </div>
-            )
-        })
-        ) : (
-        <div className="center">No posts to show</div>
-        );
-
-        return(
-            <div className="container" id="Home">
-                <h1 className="center"> Home </h1>
-                {postList}
-            </div>
+          </div>
         )
-    }
+      })
+    ) : (
+      <div className="center">No posts to show</div>
+    );
+
+    return (
+      <div>
+        <div className="container home">
+          <h4 className="center">Home</h4>
+          {postList}
+        </div>
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = (state) => {
-    return {
-        posts: state.posts
-    }
+  return {
+    posts: state.posts
+  }
 }
 
-export default connect(mapStateToProps)(Home) // connect returns a higher order function that wrapps Home 
-
-
+export default connect(mapStateToProps)(Home)
--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import roootReducer from './reducers/rootReducer'
+import rootReducer from './reducers/rootReducer'
 
-const store = createStore(roootReducer);
+const store = createStore(rootReducer);
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
