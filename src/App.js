65e5953ebcb6e4d7eb1525440d753674cdb568b9
UJJWAL CHAUDHARY <ujjwalanju2001@gmail.com>
2021-07-25 19:47:09 +0530

    hash router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import React, {Component} from 'react';
-import Navbar from './Components/Navbar';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from './Components/Home';
-import About from './Components/About';
+import React, { Component } from 'react'
+import Navbar from './Components/Navbar'
+import { HashRouter, Route, Switch } from 'react-router-dom'
+import Home from './Components/Home'
+import About from './Components/About'
 import Contact from './Components/Contact'
 import Post from './Components/Post'
 
 class App extends Component {
-  
-  render(){
-    return(
-      <BrowserRouter>
-        <div id="App">
+  render() {
+    return (
+      <HashRouter basename='/'>
+        <div id='App'>
           <Navbar />
-            <Switch>  //using switch we would only be able to hit one route at a time i.e. would only be able to show one component at a time
-              <Route exact path="/" component={Home} />
-              <Route path="/about" component={About} />
-              <Route path="/contact" component={Contact} />
-              <Route path="/:post_id" component={Post} />
-            </Switch>
+          <Switch>
+            {' '}
+            //using switch we would only be able to hit one route at a time i.e.
+            would only be able to show one component at a time
+            <Route exact path='/' component={Home} />
+            <Route path='/about' component={About} />
+            <Route path='/contact' component={Contact} />
+            <Route path='/:post_id' component={Post} />
+          </Switch>
         </div>
-      </BrowserRouter>
+      </HashRouter>
     )
   }
 }
 
-export default App;
+export default App
